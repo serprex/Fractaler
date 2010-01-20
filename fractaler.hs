547d483fdbf687f38bbb9b7be6ce362d7e64f9f3
Demur Rumed <junkmute@hotmail.com>
2010-01-20 02:36:06 +0000

    Julia and Newton arrived a tad late

--- fractaler.hs
+++ fractaler.hs
@@ -1,15 +1,19 @@
-{-# LANGUAGE BangPatterns, MagicHash #-}
+{-# LANGUAGE BangPatterns #-}
 {-# OPTIONS -fexcess-precision -funbox-strict-fields -feager-blackholing -O2 #-}
 import Graphics.UI.GLUT
 import Data.Time.Clock.POSIX(getPOSIXTime)
-import Data.StateVar
 import Data.IORef
+import Data.Complex
 import Control.Monad
 import Control.Parallel.Strategies
 import System.IO.Unsafe(unsafePerformIO)
+import GHC.Float
+import Unsafe.Coerce
+
+doubleToGF = unsafeCoerce . double2Float
 
 {-# NOINLINE func #-}
-func = unsafePerformIO $ newIORef 1000
+func = unsafePerformIO $ newIORef 10
 {-# NOINLINE xyold #-}
 xyold = unsafePerformIO $ newIORef ((-2),(2),(-2),(2))
 {-# NOINLINE xynew #-}
@@ -41,14 +45,14 @@ reshaper (Size !x !y) = do
 	ptsg $= [Vertex2 x y|x<-w,y<-w] where w=[0..fromIntegral (min x y)-1]
 
 windowLength = get windowSize >>= (\(Size w h)->return w)
-zoomAdjust (Position x y) xyo = do
+zoomAdjust (Position x y) = do
 	w <- windowLength
-	(a,b,c,d) <- get xyo
+	(a,b,c,d) <- get xyold
 	return $! (a+(fromIntegral x/fromIntegral w)*(b-a),c+(fromIntegral y/fromIntegral w)*(d-c))
-displayZoom (MouseButton LeftButton) Down _ xy = zoomAdjust xy xyold >>= (xynew$=)
+displayZoom (MouseButton LeftButton) Down _ xy = zoomAdjust xy >>= (xynew$=)
 displayZoom (MouseButton LeftButton) Up _ xy = do
 	(xn,yn) <- get xynew
-	(x,y) <- zoomAdjust xy xyold
+	(x,y) <- zoomAdjust xy
 	(x1,x2,y1,y2) <- get xyold
 	(x1,x2,y1,y2) <- return (if x==xn && y==yn then (x-x2+x1,x+x2-x1,y-y2+y1,y+y2-y1)
 		else (min x xn,max x xn,min y yn,max y yn))
@@ -64,16 +68,68 @@ displayMap = do
 	f <- get func
 	xy <- get xyold
 	t <- getPOSIXTime
-	unsafeRenderPrimitive Points $ zipWithM_ (\ !v !c->vertex v >> color c) ptsg $ parBuffer 600 rwhnf $ map (mandel f) $ pts xy w
+	unsafeRenderPrimitive Points $ zipWithM_ (\ !v !c->vertex v >> color c) ptsg $ parBuffer 600 rwhnf $ map (fractal f) $ pts xy w
 	getPOSIXTime >>= putStrLn . show . subtract t
 pts :: (Double,Double,Double,Double) -> GLsizei -> [(Double,Double)]
 pts (!x1,!x2,!y1,!y2) !wid = [(x1+((x2-x1)/w)*x,y1+((y2-y1)/w)*y)|x<-[0..w],y<-[0..w]] where w=fromIntegral wid-1
+
+fractal=newtondeg
+
+derive f x = (f (x+d)-f x)/d where d=0.0000000000005
+newraph 0 f x = x
+newraph !m f !x = if realPart(f x)^2<0.5 && realPart(f x)^2+imagPart(f x)^2 < 0.5 then x else newraph (m-1) f (x-f x/derive f x)
+newraphic 0 f !x = (x,0)
+newraphic !m f !x = if realPart(f x)^2+imagPart(f x)^2 < 0.5 then (x,m) else newraphic (m-1) f (x-f x/derive f x)
+
+newraphd 0 f g x = x
+newraphd !m f g !x = if realPart(f x)^2+imagPart(f x)^2 < 0.5 then x else newraphd (m-1) f g (x-f x/g x)
+newraphicd 0 f g !x = (x,0)
+newraphicd !m f g !x = if realPart(f x)^2+imagPart(f x)^2 < 0.5 then (x,m) else newraphicd (m-1) f g (x-f x/g x)
+
+newton :: (Complex Double -> Complex Double) -> Int -> (Double,Double) -> Color3 GLfloat
+newton f z xy = Color3 (fromIntegral zz) (doubleToGF x) (doubleToGF y)
+	where ((x:+y),zz)=newraphic z f (uncurry (:+) xy)
+newtoneg = newton (\x -> x^3-1)
+
+newtond :: (Complex Double -> Complex Double) -> (Complex Double -> Complex Double) -> Int -> (Double,Double) -> Color3 GLfloat
+newtond f g z xy = Color3 (fromIntegral zz) (doubleToGF x) (doubleToGF y)
+	where ((x:+y),zz)=newraphicd z f g (uncurry (:+) xy)
+newtondeg = newtond (\x -> x^3-1) (\x -> 3*x^2)
+
+julia :: Double -> Double -> Int -> (Double,Double) -> Color3 GLfloat
+julia x y zz (!xx,!yy) = f zz xx yy
+	where f !z !zr !zi
+		|z==0 = Color3 0 0 0
+		|zr*zr*zi*zi<4 = f (z-1) (zr*zr-zi*zi+x) (2*zr*zi+y)
+		|True = Color3 ((fromIntegral z/fromIntegral zz)^3) ((fromIntegral z/fromIntegral zz)^2) (fromIntegral z/fromIntegral zz)
+juliaeg = julia (-1) (0.2) 
+
+juliadagger :: Int -> (Double,Double) -> Color3 GLfloat
+juliadagger zz (!x,!y) = f zz x y
+	where f !z !zr !zi
+		|z==0 = Color3 0 0 0
+		|zr*zr*zi*zi<4 = f (z-1) (zr*zr-zi*zi+cos (atan2 y x)) (2*zr*zi+sin (atan2 y x))
+		|True = Color3 ((fromIntegral z/fromIntegral zz)^3) ((fromIntegral z/fromIntegral zz)^2) (fromIntegral z/fromIntegral zz)
 
 mandel :: Int -> (Double,Double) -> Color3 GLfloat
 mandel zz (!x,!y) = f zz x y
 	where f !z !zr !zi
 		|z==0 = Color3 0 0 0
-		|zr*zr+zi*zi<4 = f (z-1) (zr*zr-zi*zi+x) (2*zr*zi+y)
+		|zr*zr*zi*zi<4 = f (z-1) (zr*zr-zi*zi+x) (2*zr*zi+y)
+		|True = Color3 ((fromIntegral z/fromIntegral zz)^3) ((fromIntegral z/fromIntegral zz)^2) (fromIntegral z/fromIntegral zz)
+
+yxmandel :: Int -> (Double,Double) -> Color3 GLfloat
+yxmandel zz (!x,!y) = f zz x y
+	where f !z !zr !zi
+		|z==0 = Color3 0 0 0
+		|zr*zr+zi*zi<4 = f (z-1) (zr*zr-zi*zi+y*x) (2*zr*zi+y)
+		|True = Color3 ((fromIntegral z/fromIntegral zz)^3) ((fromIntegral z/fromIntegral zz)^2) (fromIntegral z/fromIntegral zz)
+
+nodoub :: Int -> (Double,Double) -> Color3 GLfloat
+nodoub zz (!x,!y) = f zz x y
+	where f !z !zr !zi
+		|z==0 = Color3 0 0 0
+		|zr*zr+zi*zi<4 = f (z-1) (zr*zr-zi*zi+x) (zr*zi+y)
 		|True = Color3 ((fromIntegral z/fromIntegral zz)^3) ((fromIntegral z/fromIntegral zz)^2) (fromIntegral z/fromIntegral zz)
 
 tricorn :: Int -> (Double,Double) -> Color3 GLfloat
