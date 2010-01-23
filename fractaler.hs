d9cb353ec8b2170ed2cacdf713e41bac2ff92856
Demur Rumed <junkmute@hotmail.com>
2010-01-23 17:50:46 +0000

    Added generic complex functions. Submenufied

--- fractaler.hs
+++ fractaler.hs
@@ -11,6 +11,7 @@ import System.IO.Unsafe(unsafePerformIO)
 import GHC.Float(double2Float)
 import Unsafe.Coerce(unsafeCoerce)
 
+i=(0:+1)
 {-# NOINLINE func#-}
 func = unsafePerformIO $ newIORef $ mandel
 {-# NOINLINE finc#-}
@@ -34,23 +35,35 @@ main = do
 	mouseWheelCallback $= Just detailZoom
 	attachMenu RightButton $ Menu [
 		MenuEntry "reset" $ xyold$=(-2,-2,4) >> xydrt$=True,
-		MenuEntry "mandel" $ meop mandel 100,
-		MenuEntry "newtoneg" $ meop newtoneg 5,
-		MenuEntry "juliaeg" $ meop juliaeg 100,
-		MenuEntry "tricorn" $ meop tricorn 100,
-		MenuEntry "newtontrig" $ meop newtontrig 5,
-		MenuEntry "juliadagger" $ meop juliadagger 100,
-		MenuEntry "newtontrigright" $ meop newtontrigright 5,
-		MenuEntry "burningship" $ meop burningship 100,
-		MenuEntry "nodoub" $ meop nodoub 100,
-		MenuEntry "yxmandel" $ meop yxmandel 100]
+		SubMenu "Fantou" $ Menu [
+			MenuEntry "Generic" $ meop mandel 100,
+			MenuEntry "Tricorn" $ meop tricorn 100,
+			MenuEntry "Burningship" $ meop burningship 100,
+			MenuEntry "Half I" $ meop nodoub 100,
+			MenuEntry "Dagger" $ meop dagger 100,
+			MenuEntry "XxY" $ meop yxmandel 100
+		],
+		SubMenu "Julia" $ Menu [
+			MenuEntry "-.35 .75" $ meop (julia (-0.35) 0.75) 100,
+			MenuEntry ".285 0" $ meop (julia 0.285 0) 100,
+			MenuEntry ".285 .01" $ meop (julia 0.285 0.01) 100,
+			MenuEntry "-.8 .156" $ meop (julia (-0.8) 0.156) 100
+		],
+		SubMenu "Newton" $ Menu [
+			MenuEntry "x5-1" $ meop (newton (\x->x^5-1) (\x->5*x^4)) 5,
+			MenuEntry "x5+3x3-x2-1" $ meop (newton (\x->x^5+3*x^3-x^2-1) (\x->5*x^4+9*x^2+2*x)) 5,
+			MenuEntry "(sin x)3-1" $ meop (newton (\x->sin x^3-1) (\x->3*cos x*sin x^2)) 5,
+			MenuEntry "asin" $ meop (newton asin (\x->1/sqrt(1-x*x))) 5,
+			MenuEntry "phase" $ meop (newton ((:+0) . phase) (\x->1/(1+x*x))) 5
+		],
+		SubMenu "Complex" $ Menu [
+			MenuEntry "(x2-1)(x-2-i)2/(x2+2+2i)" $ meop (complex (\x->(x^2-1)*(x-2-i)^2/(x^2+2+2*i))) 1,
+		]]
 	mainLoop
-	where meop x y = do
-		func $= x
-		finc $= y
-		xydrt $= True
+	where meop x y = func $= x >> finc $= y >> xydrt $= True
+
 detailZoom _ dir _ = do
-	fiva $~ (max 0 . (+) dir)
+	fiva $~ (max 1 . (+) dir)
 	get fiva >>= (windowTitle$=) . show
 	postRedisplay Nothing
 
@@ -71,9 +84,7 @@ displayZoom (MouseButton LeftButton) Up _ xy = do
 	if xyd then xydrt $= False else do
 		(xn,yn) <- get xynew
 		(x,y) <- zoomAdjust xy
-		xyn <- if x==xn && y==yn then do
-				(_,_,c) <- get xyold
-				return (x-c,y-c,c*2)
+		xyn <- if x==xn && y==yn then get xyold >>= (\(_,_,c)->return (x-c,y-c,c*2))
 			else return (min x xn,min y yn,max (abs $ x-xn) (abs $ y-yn))
 		xyold $= xyn
 		windowTitle $= show xyn
@@ -89,37 +100,26 @@ displayMap = do
 	func <- get func
 	finc <- get finc
 	fiva <- get fiva
-	unsafeRenderPrimitive Points $ zipWithM_ (\v c->vertex v >> color c) [Vertex2 a b|a<-[0..w],b<-[0..w]] $ parBuffer 600 rwhnf $ map (func (fiva*finc)) $ pts xy w
+	unsafeRenderPrimitive Points $ zipWithM_ (\v c->vertex v >> color c) [Vertex2 a b|a<-[0..w],b<-[0..w]] $ parBuffer 600 rwhnf $ map (func $ fiva*finc) $ pts xy w
 	getPOSIXTime >>= putStrLn . show . subtract t
 
 doubleToGF = unsafeCoerce . double2Float
-hsvrgb :: Double -> Double -> Double -> Color3 GLfloat
-hsvrgb !h !s !v = let
-	p=doubleToGF $ v-s*v
-	q=doubleToGF $ v-6*h*s*v
-	t=doubleToGF $ v-s*v+6*h*s*v
-	x=doubleToGF v
-	in case mod (floor $ 6*h) 6 of
-		0->Color3 x t p
-		1->Color3 q x p
-		2->Color3 p x t
-		3->Color3 p q x
-		4->Color3 t p x
-		5->Color3 x p q
-
+hvrgb :: Complex Double -> Double -> Color3 GLfloat
+hvrgb !hc !v = Color3 (l 0) (l 4) (l 2) where
+	h=3+phase hc*3/pi
+	l=doubleToGF . (!!) [v,v-h*v*v,0,0,h*v,v] . (flip rem) 6 . (+(truncate h))
 magsqr,magnitude :: Complex Double -> Double
 magsqr (a:+b) = a*a+b*b
 magnitude = sqrt . magsqr
 
-newraph f g 0 x = (x,0)
-newraph f g m !x = if magnitude(f x)<0.00000005 then (x,m) else newraph f g (m-1) (x-f x/g x)
+complex :: (Complex Double -> Complex Double) -> Int -> Complex Double -> Color3 GLfloat
+complex f z xy = hvrgb (f xy) $ logBase (fromIntegral z) $ magnitude (f xy)+1
 
 newton :: (Complex Double -> Complex Double) -> (Complex Double -> Complex Double) -> Int -> Complex Double -> Color3 GLfloat
-newton f g z xy = hsvrgb (0.5+atan2 y x/(2*pi)) 1 (fromIntegral zz/fromIntegral z)
-	where (x:+y,zz)=newraph f g z xy
-newtoneg = newton (\x -> x^5-1) (\x -> 5*x^4)
-newtontrig = newton (\x -> sin x^3-1) (\x -> 3*cos x^2)
-newtontrigright = newton (\x -> sin x^3-1) (\x -> 3*cos x*sin x^2)
+newton f g z xy = hvrgb (x:+y) (fromIntegral zz/fromIntegral z)
+	where
+		newraph f g m x = if m==0 then (x,0) else if magsqr(f x)<1/fromIntegral m then (x,m) else newraph f g (m-1) (x-f x/g x)
+		(x:+y,zz)=newraph f g z xy
 
 julia :: Double -> Double -> Int -> Complex Double -> Color3 GLfloat
 julia x y zz (xx:+yy) = f zz xx yy
@@ -127,10 +127,9 @@ julia x y zz (xx:+yy) = f zz xx yy
 		|z==0 = Color3 0 0 0
 		|zr*zr*zi*zi<4 = f (z-1) (zr*zr-zi*zi+x) (2*zr*zi+y)
 		|True = Color3 ((fromIntegral z/fromIntegral zz)^3) ((fromIntegral z/fromIntegral zz)^2) (fromIntegral z/fromIntegral zz)
-juliaeg = julia (-0.35) 0.75
 
-juliadagger :: Int -> Complex Double -> Color3 GLfloat
-juliadagger zz (x:+y) = f zz x y
+dagger :: Int -> Complex Double -> Color3 GLfloat
+dagger zz (x:+y) = f zz x y
 	where f z zr zi
 		|z==0 = Color3 0 0 0
 		|zr*zr*zi*zi<4 = f (z-1) (zr*zr-zi*zi+cos (atan2 y x)) (2*zr*zi+sin (atan2 y x))
